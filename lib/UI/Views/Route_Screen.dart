8eec94069fa01c97e748b263dea2902434f76c7f
Aaroh Sharma <44449261+MrAwesome9140@users.noreply.github.com>
2021-06-24 14:21:15 -0500

    Added route settings

--- lib/UI/Views/Route_Screen.dart
+++ lib/UI/Views/Route_Screen.dart
@@ -1,7 +1,9 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:material_floating_search_bar/material_floating_search_bar.dart';
 import 'package:navigation_app/UI/Views/Search_Screen.dart';
 import 'package:navigation_app/State/route_store.dart';
+import 'package:flutter_switch/flutter_switch.dart';
 
 class RouteScreen extends StatefulWidget {
   @override
@@ -10,6 +12,8 @@ class RouteScreen extends StatefulWidget {
 
 class _RouteScreenState extends State<RouteScreen> {
   RouteStore _routeStore = RouteStore();
+  bool _switch1State = false;
+  bool _switch2State = false;
 
   @override
   Widget build(BuildContext context) {
@@ -17,84 +21,174 @@ class _RouteScreenState extends State<RouteScreen> {
     return Scaffold(
       body: Stack(
         children: [
-          Column(
-            children: [
-              Padding(
-                padding: EdgeInsets.only(
-                  top: size.height * 0.3,
-                ),
-                child: Container(
-                  height: size.height * 0.6,
-                  child: ReorderableListView(
-                    children: List<Widget>.generate(_routeStore.route.length,
-                        (index) {
-                      return ListTile(
-                        leading: Icon(Icons.location_pin),
-                        title: Text(''),
-                      );
-                    }),
-                    onReorder: (before, after) {},
-                  ),
-                ),
-              )
-            ],
+          Padding(
+            padding: EdgeInsets.only(
+              top: size.height * 0.15,
+            ),
+            child: Column(
+              children: [
+                _routeSettings(size),
+                _locationsDisplay(size),
+              ],
+            ),
           ),
-          Container(
-            child: Hero(
-              tag: 'SearchBar',
-              child: GestureDetector(
-                behavior: HitTestBehavior.translucent,
-                onTap: () {
-                  Navigator.of(context).push(MaterialPageRoute(
-                    builder: (_) => SearchScreen(),
-                  ));
-                },
-                child: Align(
-                  alignment: Alignment.topCenter,
-                  child: Padding(
-                    padding: EdgeInsets.only(top: size.height * 0.03),
-                    child: IgnorePointer(
-                      child: FloatingSearchBar(
-                        hint: 'Add Locations...',
-                        scrollPadding: EdgeInsets.only(top: 16, bottom: 56),
-                        transitionDuration: const Duration(milliseconds: 800),
-                        transitionCurve: Curves.easeInOut,
-                        transition: ExpandingFloatingSearchBarTransition(),
-                        physics: const BouncingScrollPhysics(),
-                        height: size.height * 0.06,
-                        width: size.width * 0.85,
-                        debounceDelay: const Duration(milliseconds: 500),
-                        onQueryChanged: (query) {},
-                        onFocusChanged: (focus) {
-                          Navigator.of(context).push(MaterialPageRoute(
-                            builder: (_) => SearchScreen(),
-                          ));
-                        },
-                        leadingActions: [],
-                        builder: (context, transition) {
-                          return ClipRRect(
-                            borderRadius: BorderRadius.circular(8),
-                            child: Material(
-                              color: Colors.white,
-                              elevation: 4.0,
-                              child: Column(
-                                mainAxisSize: MainAxisSize.min,
-                                children: Colors.accents.map((color) {
-                                  return Container(height: 112, color: color);
-                                }).toList(),
-                              ),
-                            ),
-                          );
-                        },
-                      ),
-                    ),
+          _searchBar(size),
+        ],
+      ),
+    );
+  }
+
+  Widget _locationsDisplay(Size size) {
+    return Container(
+      decoration: BoxDecoration(color: Colors.grey),
+      height: size.height * 0.3,
+      child: ReorderableListView(
+        children: List<Widget>.generate(_routeStore.route.length, (index) {
+          return Container(
+            height: size.height * 0.09,
+            child: ListTile(
+              tileColor: Colors.blue,
+              leading: Icon(Icons.location_pin),
+              title: Text(''),
+            ),
+          );
+        }),
+        onReorder: (before, after) {},
+      ),
+    );
+  }
+
+  Widget _searchBar(Size size) {
+    return Padding(
+      padding: EdgeInsets.only(top: size.height * 0.03),
+      child: Container(
+        height: size.height * 0.09,
+        child: Hero(
+          tag: 'SearchBar',
+          child: Align(
+            alignment: Alignment.topCenter,
+            child: GestureDetector(
+              behavior: HitTestBehavior.deferToChild,
+              onTap: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                  builder: (_) => SearchScreen(),
+                ));
+              },
+              child: Container(
+                height: size.height * 0.09,
+                child: AbsorbPointer(
+                  child: FloatingSearchBar(
+                    hint: 'Add Locations...',
+                    scrollPadding: EdgeInsets.only(top: 16, bottom: 56),
+                    transitionDuration: const Duration(milliseconds: 800),
+                    transitionCurve: Curves.easeInOut,
+                    transition: ExpandingFloatingSearchBarTransition(),
+                    physics: const BouncingScrollPhysics(),
+                    height: size.height * 0.06,
+                    width: size.width * 0.85,
+                    debounceDelay: const Duration(milliseconds: 500),
+                    onQueryChanged: (query) {},
+                    onFocusChanged: (focus) {
+                      Navigator.of(context).push(MaterialPageRoute(
+                        builder: (_) => SearchScreen(),
+                      ));
+                    },
+                    leadingActions: [],
+                    builder: (context, transition) {
+                      return ClipRRect(
+                        borderRadius: BorderRadius.circular(8),
+                        child: Material(
+                          color: Colors.white,
+                          elevation: 4.0,
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: Colors.accents.map((color) {
+                              return Container(height: 112, color: color);
+                            }).toList(),
+                          ),
+                        ),
+                      );
+                    },
                   ),
                 ),
               ),
             ),
           ),
-        ],
+        ),
       ),
     );
+  }
+
+  Widget _routeSettings(Size size) {
+    return Column(children: [
+      Padding(
+        padding: EdgeInsets.only(top: size.height * 0.03),
+        child: SizedBox(
+          height: size.height * 0.07,
+          width: size.width * 0.85,
+          child: Text(
+            'Route Settings',
+            style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.w600),
+          ),
+        ),
+      ),
+      Padding(
+        padding: EdgeInsets.only(),
+        child: Column(
+          children: [
+            Container(
+              height: size.height * 0.06,
+              width: size.width * 0.9,
+              child: ListTile(
+                leading: Icon(Icons.settings),
+                title: Container(
+                  width: size.width * 0.6,
+                  child: Text(
+                    'Optimize Route',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.w300,
+                    ),
+                  ),
+                ),
+                trailing: Switch(
+                  value: _switch1State,
+                  onChanged: (val) {
+                    setState(() {
+                      _switch1State = val;
+                    });
+                  },
+                ),
+              ),
+            ),
+            Container(
+              height: size.height * 0.06,
+              width: size.width * 0.9,
+              child: ListTile(
+                leading: Icon(Icons.location_pin),
+                title: Container(
+                  width: size.width * 0.6,
+                  child: Text(
+                    'End at Start Location',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.w300,
+                    ),
+                  ),
+                ),
+                trailing: Switch(
+                  value: _switch2State,
+                  onChanged: (val) {
+                    setState(() {
+                      _switch2State = val;
+                    });
+                  },
+                ),
+              ),
+            ),
+          ],
+        ),
+      )
+    ]);
   }
 }
